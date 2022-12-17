1123b093ab6fe2c802288bbaf73ca2ae87f09c95
Rei-L0 <qkrdusgn00@naver.com>
2022-12-18 01:00:17 +0900

    add google map

--- app/src/main/java/com/example/finalterm/MainActivity.kt
+++ app/src/main/java/com/example/finalterm/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.finalterm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalterm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
